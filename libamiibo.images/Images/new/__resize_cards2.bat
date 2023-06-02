6efa20be26dab9ab191af4c03ea5697f06e4d7cc
DarkIrata <git@ipmix.de>
2023-06-02 18:01:21 +0200

    Removed Images Project

--- libamiibo.images/Images/new/__resize_cards2.bat
+++
@@ -1,3 +0,0 @@
-convert *.png -set filename:original %t -background none -gravity south -extent 453x343 %[filename:original].png
-convert *.png -set filename:original %t -background none -gravity center -extent 453x453 %[filename:original].png
-convert *.png -set filename:original %t -resize 256x256 %[filename:original].png
